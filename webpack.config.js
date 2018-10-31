c5c6ae40646cf4e32c87eb46f7d7c421eb72aaaa
pedro.sorto <pedro.sorto@radiantsolutions.com>
2018-10-30 23:27:43 -0400

    Resolve issue with babel loader not including react stage-0

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,10 @@ module.exports = {
       }, {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react', 'stage-0']
+        }
       }, {
         test: /\.css$/,
         use: [
